119fdfdd7a101314309bd91436c73f6f8a09764d
Anna Kashtanova <annakawtanova@gmail.com>
2023-07-30 17:44:59 -0400

    add currency dropdown

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,7 +3,7 @@ import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
   const { budget, dispatch } = useContext(AppContext);
-  const { expenses } = useContext(AppContext);
+  const { expenses, currency } = useContext(AppContext);
 
   const setBudgetHandle = (event) => {
     // Update the budget directly with the input value
@@ -33,7 +33,7 @@ const Budget = () => {
 
   return (
     <div className='alert alert-secondary'>
-      <span style={{ marginRight: '0.5rem' }}>Budget: £</span>
+      <span style={{ marginRight: '0.5rem' }}>Budget: {currency}</span>
       <input
         required='required'
         type='number'
--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,50 +1,45 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
-const Budget = () => {
-  const { budget, dispatch } = useContext(AppContext);
-  const { expenses } = useContext(AppContext);
+const Currency = () => {
+  const { currency, dispatch } = useContext(AppContext);
 
-  const setBudgetHandle = (event) => {
+  const setCurrency = (event) => {
     // Update the budget directly with the input value
-    const inputValue = parseFloat(event.target.value);
-    
-    const totalExpenses = expenses.reduce((total, item) => {
-        return (total = total + item.cost);
-    }, 0);
+    const nextCurrency = event.target.value;
 
-    if(inputValue < totalExpenses) {
-        alert("You cannot reduce the budget value lower then the spending");
-
-        return;
-    }
-
-    if(inputValue > 20000) {
-        alert("The value cannot exceed 20000£");
-
+    if (currency === nextCurrency){
         return;
     }
 
     dispatch({
       type: 'CHG_CURRENCY',
-      payload: inputValue,
+      payload: nextCurrency,
     });
+  };
+
+  const signToCurrency = (cur) => {
+    if (cur === '$') return "Dollar";
+    else if (cur === '€') return "Euro";
+    else if (cur === '₹') return "Ruppee";
+    else if (cur === '£') return "Pound";
+    throw new Error("Unexpected currency:" + cur);
   };
 
   return (
     <div className='alert alert-secondary'>
-      <span style={{ marginRight: '0.5rem' }}>Budget: £</span>
-      <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
-                <option defaultValue>($ Dollar)</option>
-                <option value="Marketing" name="marketing"> Marketing</option>
-                <option value="Sales" name="sales">Sales</option>
-                <option value="Finance" name="finance">Finance</option>
-                <option value="HR" name="hr">HR</option>
-                <option value="IT" name="it">IT</option>
-                <option value="Admin" name="admin">Admin</option>
+      <span style={{ marginRight: '0.5rem' }}>Currency:</span>
+      (
+      <select className="custom-select" id="inputGroupSelect04" onChange={setCurrency}>
+                <option defaultValue value={currency}>{currency} {signToCurrency(currency)}</option>
+                <option value="$" name="dollar">$ Dollar</option>
+                <option value="£" name="pound">£ Pound</option>
+                <option value="€" name="euro">€ Euro</option>
+                <option value="₹" name="ruppee">₹ Ruppee</option>
         </select>
+      )
     </div>
   );
 };
 
-export default Budget;
+export default Currency;
